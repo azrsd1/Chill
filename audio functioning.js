2eac7a13227fb4582737ebba9baede16a5353263
39D■ <hpoje@icloud.com>
2017-06-19 20:47:40 +0300

    Update audio functioning.js

--- audio functioning.js
+++ audio functioning.js
@@ -5,10 +5,12 @@ function playAudio() {
 		music.play();
 		pbutton.className = "";
 		pbutton.className = "pause";
+		document.getElementById('radio').src='img/radioOn.gif';
 		
 	} else {
 		music.pause();
 		pbutton.className = "";
 		pbutton.className = "play";
+		document.getElementById('radio').src='img/radioOff.png';
 	}
 }
