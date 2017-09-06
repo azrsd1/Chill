d16dc520419313ddfceb1c07ffc1e34a0e8bf283
39D■ <hpoje@icloud.com>
2017-09-06 14:09:13 +0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -6,7 +6,15 @@ function playAudio() {
 		pbutton.className = "";
 		pbutton.className = "pause";
 		document.getElementById('radio').src='img/radioOn.gif';
-		
+		if(music.currentTime >= 0 && music.currentTime < 118){
+			document.getElementById('artist-name').innerHTML = 'eery - her';}
+		else if(music.currentTime > 118 && music.currentTime < 270){document.getElementById('artist-name').innerHTML = 'Bunko Stew - Hide The Pain';}
+		else if(music.currentTime > 270 && music.currentTime < 505){document.getElementById('artist-name').innerHTML = 'elijah - sad and boujee';}
+		else if(music.currentTime > 505 && music.currentTime < 758){document.getElementById('artist-name').innerHTML = 'saib - in your arms';}
+		else if(music.currentTime > 758 && music.currentTime < 893){document.getElementById('artist-name').innerHTML = 'Nohidea - Thirteen (w/Idealism)';}
+		else if(music.currentTime > 893 && music.currentTime < 1040){document.getElementById('artist-name').innerHTML = 'atlas - sand (prod. tomppabeats)';}
+		else if(music.currentTime > 1040 && music.currentTime < 1150){document.getElementById('artist-name').innerHTML = 'potsu - im closing my eyes (ft. shiloh)';}
+		else if(music.currentTime > 1150){document.getElementById('artist-name').innerHTML = 'eery - off-white';}
 	} else {
 		music.pause();
 		pbutton.className = "";
@@ -16,13 +24,50 @@ function playAudio() {
 }
 
 function playNext(){
-	if (music.currentTime < 15){music.currentTime = 120;}
+	if (music.currentTime < 118){music.currentTime = 118;}
+	else if(music.currentTime < 270){music.currentTime = 270;}
+	else if(music.currentTime < 505){music.currentTime = 505;}
+	else if(music.currentTime < 758){music.currentTime = 758;}
+	else if(music.currentTime < 893){music.currentTime = 893;}
+	else if(music.currentTime < 1040){music.currentTime = 1040;}
+	else if(music.currentTime < 1150){music.currentTime = 1150;}
+	else if(music.currentTime > 1150){
+	
+	music.currentTime = 1292;
+	pbutton.className = "play";
+		document.getElementById('radio').src='img/radioOff.png';}
 
 }
 
+function playPrevious(){
+	if (music.currentTime < 118){music.currentTime = 0;}
+	else if(music.currentTime > 118 && music.currentTime < 270){music.currentTime = 0;}
+	else if(music.currentTime > 270 && music.currentTime < 505){music.currentTime = 118;}
+	else if(music.currentTime > 505 && music.currentTime < 758){music.currentTime = 270;}
+	else if(music.currentTime > 758 && music.currentTime < 893){music.currentTime = 505;}
+	else if(music.currentTime > 893 && music.currentTime < 1040){music.currentTime = 758;}
+	else if(music.currentTime > 1040 && music.currentTime < 1150){music.currentTime = 893;}
+	else if(music.currentTime > 1150){music.currentTime = 1040;}
+
+}
+
+
+music.ontimeupdate = function(){
+	if(music.currentTime >= 0 && music.currentTime < 118){
+			document.getElementById('artist-name').innerHTML = 'eery - her';}
+	else if(music.currentTime > 118 && music.currentTime < 270){document.getElementById('artist-name').innerHTML = 'Bunko Stew - Hide The Pain';}
+	else if(music.currentTime > 270 && music.currentTime < 505){document.getElementById('artist-name').innerHTML = 'elijah - sad and boujee';}
+	else if(music.currentTime > 505 && music.currentTime < 758){document.getElementById('artist-name').innerHTML = 'saib - in your arms';}
+	else if(music.currentTime > 758 && music.currentTime < 893){document.getElementById('artist-name').innerHTML = 'Nohidea - Thirteen (w/Idealism)';}
+	else if(music.currentTime > 893 && music.currentTime < 1040){document.getElementById('artist-name').innerHTML = 'atlas - sand (prod. tomppabeats)';}
+	else if(music.currentTime > 1040 && music.currentTime < 1150){document.getElementById('artist-name').innerHTML = 'potsu - im closing my eyes (ft. shiloh)';}
+	else if(music.currentTime > 1150){document.getElementById('artist-name').innerHTML = 'eery - off-white';}
+};
+
 music.onended = function() {
    pbutton.className = "";
-		pbutton.className = "play";
-		document.getElementById('radio').src='img/radioOff.png';
+   pbutton.className = "play";
+   document.getElementById('radio').src='img/radioOff.png';
+   document.getElementById('artist-name').innerHTML = '';
 };
 
